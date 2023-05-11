4b54aec1b7485e60ef22fd594f471ace67278d13
Saharsh0 <113522933+Saharsh0@users.noreply.github.com>
2023-05-11 12:05:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 //https://teachablemachine.withgoogle.com/models/esgBR3bNO/
+
+startClassification()
+{
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/esgBR3bNO/',modelReady)
+}
+
+modelReady()
+{
+    classifier.classify(gotResults);
+}
+
+gotResults(error, results)
+{
+    console.log(gotResult)
+}
